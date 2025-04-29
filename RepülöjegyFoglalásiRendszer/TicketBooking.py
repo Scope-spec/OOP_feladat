ff87640135fab4f645c467f5842f50300e0194d3
David Priegl <davidpriegl@gmail.com>
2025-04-29 16:30:29 +0200

    commit verzio 0.1

--- RepülöjegyFoglalásiRendszer/TicketBooking.py
+++
@@ -1,4 +0,0 @@
-from Domestic import Domestic
-
-j1 = Domestic("N30", "Budapest", 400)
-print(j1.info())
---
+++ RepülöjegyFoglalásiRendszer/main.py
@@ -0,0 +1,24 @@
+
+def felhasznaloi_felulet():
+    while True:
+        print("\n--- Repülőjegy Foglalási Rendszer ---")
+        print("1. Jegy foglalása")
+        print("2. Foglalás lemondása")
+        print("3. Foglalások listázása")
+        print("4. Elérhető járatok")
+        print("0. Kilépés")
+
+        valasztas = input("Válassz egy műveletet: ")
+
+        if valasztas == "1":
+
+        elif valasztas == "2":
+
+        elif valasztas == "3":
+
+        elif valasztas == "4":
+
+        elif valasztas == "0":
+            print("Kilépés a rendszerből.")
+            break
+
