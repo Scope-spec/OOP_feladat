fe86cec7789d963bce23a382d31f7cd0f74d7dab
David Priegl <davidpriegl@gmail.com>
2025-04-29 12:45:24 +0200

    commit Domestic.py

---
+++ RepülöjegyFoglalásiRendszer/Domestic.py
@@ -0,0 +1,9 @@
+from Route import Route
+
+class Domestic(Route):
+    def __init__(self, id, destination, ticket_price):
+        super().__init__(id, destination, ticket_price)
+
+    def info(self):
+        return f"Route number: {self.get_id}, Destination: {self.get_destination}, Ticket price: {self.get_ticket_price}"
+
